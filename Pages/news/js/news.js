24f1216e899a63ea396a1ca9212d4449c590a520
Amr Hassan <amr_hassan522@yahoo.com>
2020-10-21 12:50:17 +0200

    comment firebase from news

--- Pages/news/js/news.js
+++ Pages/news/js/news.js
@@ -1,51 +1,51 @@
-var postBtn = document.getElementById("postNow")
-var postTitle = document.getElementById("posttitle")
-var postBody = document.getElementById("postbody")
+// var postBtn = document.getElementById("postNow")
+// var postTitle = document.getElementById("posttitle")
+// var postBody = document.getElementById("postbody")
 
-postBtn.addEventListener('click', function() {
-    rootRef.push({
-        PostTitle: postTitle.value,
-        postBody: postBody.value
-    });
-})
-
-
-var firebaseConfig = {
-    apiKey: "AIzaSyDE8wIHtR3kBY77emV6ktQHl1Mz9WQMN-k",
-    authDomain: "nothing-real.firebaseapp.com",
-    databaseURL: "https://nothing-real.firebaseio.com",
-    projectId: "nothing-real",
-    storageBucket: "nothing-real.appspot.com",
-    messagingSenderId: "655794930116",
-    appId: "1:655794930116:web:a6d7a1dfd85052a92be491",
-    measurementId: "G-9NLPPBLQWL"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+// postBtn.addEventListener('click', function() {
+//     rootRef.push({
+//         PostTitle: postTitle.value,
+//         postBody: postBody.value
+//     });
+// })
 
 
-// var test = document.getElementById("test")
-// rootRef.on('value', snap => test.innerText = snap.val());
+// var firebaseConfig = {
+//     apiKey: "AIzaSyDE8wIHtR3kBY77emV6ktQHl1Mz9WQMN-k",
+//     authDomain: "nothing-real.firebaseapp.com",
+//     databaseURL: "https://nothing-real.firebaseio.com",
+//     projectId: "nothing-real",
+//     storageBucket: "nothing-real.appspot.com",
+//     messagingSenderId: "655794930116",
+//     appId: "1:655794930116:web:a6d7a1dfd85052a92be491",
+//     measurementId: "G-9NLPPBLQWL"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
 
-const rootRef = firebase.database().ref("issues/");
-rootRef.on("value",
 
-    (snapshot) => {
-        const listtablebody = document.getElementById("recent");
-        listtablebody.textContent = "";
-        snapshot.forEach((child) => {
-            issue = child.val();
-            console.log(issue)
+// // var test = document.getElementById("test")
+// // rootRef.on('value', snap => test.innerText = snap.val());
+
+// const rootRef = firebase.database().ref("issues/");
+// rootRef.on("value",
+
+//     (snapshot) => {
+//         const listtablebody = document.getElementById("recent");
+//         listtablebody.textContent = "";
+//         snapshot.forEach((child) => {
+//             issue = child.val();
+//             console.log(issue)
 
 
-            var post = document.createElement("div")
-            post.innerHTML = "<h3>" + issue.PostTitle + "</h3> <h5>" + issue.postBody + "</h5>"
-            listtablebody.append(post)
-        })
+//             var post = document.createElement("div")
+//             post.innerHTML = "<h3>" + issue.PostTitle + "</h3> <h5>" + issue.postBody + "</h5>"
+//             listtablebody.append(post)
+//         })
 
-    }
-)
+//     }
+// )
 
 
 
