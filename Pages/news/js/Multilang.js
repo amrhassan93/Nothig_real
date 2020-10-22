2508d050c9ff2c49e3a5844daef3dd2ad89013ea
Amr Hassan <amr_hassan522@yahoo.com>
2020-10-22 23:01:27 +0200

    fix lang bug + Footer post design on news

--- Pages/news/js/Multilang.js
+++ Pages/news/js/Multilang.js
@@ -1,3 +1,23 @@
+$(function() {
+    $('.translate').click(function() {
+        // save, in your .translate click handler
+        localStorage.setItem('lang', $(this).attr('id'));
+        var lang = localStorage.getItem('lang') || 'en';
+
+        $('.lang').each(function(index, element) {
+            $(this).text(arrLang[lang][$(this).attr('key')])
+        })
+    })
+
+    var lang = localStorage.getItem('lang') || 'en';
+
+    $('.lang').each(function(index, element) {
+        $(this).text(arrLang[lang][$(this).attr('key')])
+    })
+
+})
+
+
 var arrLang = {
     "en": {
         "HOME": "Home",
